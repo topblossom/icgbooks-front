24ff02098b6fb29a99a479e38c6d388939dcb590
Jacek Palczewski <jpalczewski@gmail.com>
2020-05-27 00:10:38 +0200

    possible deployment fix

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -6,7 +6,7 @@ const OfflinePlugin = require('offline-plugin');
 const { HashedModuleIdsPlugin } = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-
+const Dotenv = require('dotenv-webpack');
 module.exports = require('./webpack.base.babel')({
   mode: 'production',
 
@@ -66,6 +66,7 @@ module.exports = require('./webpack.base.babel')({
   },
 
   plugins: [
+    new Dotenv({ silent: false }),
     // Minify and optimize the index.html
     new HtmlWebpackPlugin({
       template: 'app/index.html',
